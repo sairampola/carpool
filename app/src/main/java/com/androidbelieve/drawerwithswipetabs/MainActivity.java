5f3cb628a5a99598ff672dc89a2a0dbb0e645483
sairampola <sairampola6@gmail.com>
2017-02-07 11:33:31 +0530

    added runtime permisson

--- app/src/main/java/com/androidbelieve/drawerwithswipetabs/MainActivity.java
+++ app/src/main/java/com/androidbelieve/drawerwithswipetabs/MainActivity.java
@@ -1,11 +1,16 @@
 package com.androidbelieve.drawerwithswipetabs;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
@@ -42,6 +47,15 @@ public class MainActivity extends AppCompatActivity implements
         final SharedPreferences prefs = getSharedPreferences("carpool", MODE_PRIVATE);
          final int status = prefs.getInt("status",0);
 
+
+        /////////////runtime permission////////////
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            //ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},2);
+        }
+
+        /////////////------runtime permission//////
+
         /**
          *Setup the DrawerLayout and NavigationView
          */
@@ -62,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements
          */
 
 
-        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         if(status==0){
             FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.containerView,new LoginFragment()).commit();
@@ -196,5 +210,40 @@ public class MainActivity extends AppCompatActivity implements
                 "Google Places API connection failed with error code:" +
                         connectionResult.getErrorCode(),
                 Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            case 2: {
+
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Permission denied to Access your Location", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+
+        }
     }
 }
