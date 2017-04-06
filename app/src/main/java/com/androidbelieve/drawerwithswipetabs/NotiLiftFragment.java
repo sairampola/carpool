3c1a2066654177c63f8c8c06957277b2cbc8c5b6
sairampola <sairampola6@gmail.com>
2017-04-06 18:31:14 +0530

    done with notifications

--- app/src/main/java/com/androidbelieve/drawerwithswipetabs/NotiLiftFragment.java
+++ app/src/main/java/com/androidbelieve/drawerwithswipetabs/NotiLiftFragment.java
@@ -1,17 +1,47 @@
 package com.androidbelieve.drawerwithswipetabs;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by Ratan on 7/29/2015.
  */
 public class NotiLiftFragment extends Fragment {
 
+    private static final String TAG = "Cap";
+
+    // Movies json url
+    private static final String url = AGlobal.url+"notiliftresponse";
+    private ProgressDialog pDialog;
+    private final List<NotiPickupResults> movieList = new ArrayList<NotiPickupResults>();
+    private ListView listView;
+    private  NotiLiftListAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,5 +52,115 @@ public class NotiLiftFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ((MainActivity)getActivity()).setlolbar("Notifications");
+        final SharedPreferences prefs = getContext().getSharedPreferences("carpool", MODE_PRIVATE);
+
+        listView = (ListView) view.findViewById(R.id.list);
+        adapter = new NotiLiftListAdapter(getActivity(), movieList,getContext(),getFragmentManager());
+        listView.setAdapter(adapter);
+
+        pDialog = new ProgressDialog(getActivity());
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        // changing action bar color
+        //getActivity().getActionBar().setBackgroundDrawable(
+        //new ColorDrawable(Color.parseColor("#1b1b1b")));
+
+        // Creating volley request obj
+        StringRequest movieReq = new StringRequest(Request.Method.POST,url,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String respons) {
+                        Log.d(TAG, respons);
+                        hidePDialog();
+                        JSONArray response = null;
+                        try {
+                            response = new JSONArray(respons);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                NotiPickupResults movie = new NotiPickupResults();
+                                if(Integer.parseInt(obj.getString("Status"))==2)
+                                    continue;
+                                movie.setTitle(obj.getString("Aname"));
+                                movie.setThumbnailUrl(obj.getString("Apropic"));
+                                movie.setAemailid(obj.getString("Aemail"));
+                                movie.setBemailid(obj.getString("Bemail"));
+                                movie.setStatus(obj.getString("Status"));
+                                //movie.setEmailid(obj.getString("email"));
+                                //movie.setYear(obj.getInt("seats"));
+
+                                // Genre is json array
+                                    /*JSONArray genreArry = obj.getJSONArray("genre");
+                                    ArrayList<String> genre = new ArrayList<String>();
+                                    for (int j = 0; j < genreArry.length(); j++) {
+                                        genre.add((String) genreArry.get(j));
+                                    }
+                                    movie.setGenre(genre);*/
+
+                                // adding movie to movies array
+                                movieList.add(movie);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        Collections.reverse(movieList);
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidePDialog();
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                final Map<String, String> params = new HashMap<String, String>();
+                //params.put("name", "Androidhive");
+
+                //params.put("gender", AGlobal.gen);
+                //params.put("seats", String.valueOf(AGlobal.nos));
+                //params.put("name", AGlobal.na);
+                //params.put("user_img", AGlobal.ui);
+                params.put("Aemail", prefs.getString("email",null));
+                //params.put("source_address", AGlobal.sa);
+                //params.put("destination_address", AGlobal.da);
+                //Long lo = System.currentTimeMillis()/1000;
+                //params.put("timestamp",lo.toString());
+
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(movieReq);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hidePDialog();
+    }
+
+    private void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
     }
 }
---
+++ app/src/main/java/com/androidbelieve/drawerwithswipetabs/NotiLiftListAdapter.java
@@ -0,0 +1,268 @@
+package com.androidbelieve.drawerwithswipetabs;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * Created by pain on 6/4/17.
+ */
+
+public class NotiLiftListAdapter extends BaseAdapter {
+    private Activity activity;
+    private LayoutInflater inflater;
+    private Context con;
+    private FragmentManager fml;
+    private String emll;
+    private List<NotiPickupResults> movieItems;
+    NotiPickupResults m;
+    ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+
+    public NotiLiftListAdapter(Activity activity, List<NotiPickupResults> movieItems,Context con,FragmentManager fm) {
+        this.activity = activity;
+        this.movieItems = movieItems;
+        this.con = con;
+        this.fml=fm;
+    }
+
+    @Override
+    public int getCount() {
+        return movieItems.size();
+    }
+
+    @Override
+    public Object getItem(int location) {
+        return movieItems.get(location);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = inflater.inflate(R.layout.noti_lift_row_layout, null);
+
+        if (imageLoader == null)
+            imageLoader = AppController.getInstance().getImageLoader();
+       // NetworkImageView thumbNail = (NetworkImageView) convertView
+                //.findViewById(R.id.thumbnail);
+        TextView title = (TextView) convertView.findViewById(R.id.name2);
+        //final Button acc = (Button)convertView.findViewById(R.id.Accept);
+        //final Button rej = (Button)convertView.findViewById(R.id.Reject);
+        m = movieItems.get(position);
+        final String url2 = AGlobal.url;
+        final SharedPreferences prefs = con.getSharedPreferences("carpool", MODE_PRIVATE);
+
+
+/*
+        rej.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+/////////////////////////////////////////////////////////////
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url2+"notiacceptreject",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                // Result handling
+                                //Toast.makeText(getContext(),response,Toast.LENGTH_SHORT).show();
+                                try {
+                                    Toast.makeText(con,"Rejected!!",Toast.LENGTH_SHORT).show();
+
+                                    rej.setVisibility(View.GONE);
+                                    acc.setVisibility(View.VISIBLE);
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        // Error handling
+
+                        //System.out.println("Something went wrong!");
+                        error.printStackTrace();
+
+                    }
+
+                }){
+                    @Override
+                    protected Map<String, String> getParams() {
+                        final Map<String, String> params = new HashMap<String, String>();
+                        //params.put("name", "Androidhive");
+
+                        params.put("Aname", m.getTitle());
+                        params.put("Aemail", m.getAemailid());
+                        params.put("Bemail", m.getBemailid());
+                        params.put("Status", "1");
+                        return params;
+                    }
+
+                };
+
+// Add the request to the queue
+                Volley.newRequestQueue(con).add(stringRequest);
+
+
+
+
+
+
+
+                //////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
+
+            }
+        });
+
+
+        acc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+/////////////////////////////////////////////////////////////
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url2+"notiacceptreject",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                // Result handling
+                                //Toast.makeText(getContext(),response,Toast.LENGTH_SHORT).show();
+                                try {
+                                    Toast.makeText(con,"Accepted!!",Toast.LENGTH_SHORT).show();
+
+                                    acc.setVisibility(View.GONE);
+                                    rej.setVisibility(View.VISIBLE);
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        // Error handling
+
+                        //System.out.println("Something went wrong!");
+                        error.printStackTrace();
+
+                    }
+
+                }){
+                    @Override
+                    protected Map<String, String> getParams() {
+                        final Map<String, String> params = new HashMap<String, String>();
+                        //params.put("name", "Androidhive");
+
+                        params.put("Aname", m.getTitle());
+                        params.put("Aemail", m.getAemailid());
+                        params.put("Bemail", m.getBemailid());
+                        params.put("Status", "0");
+                        return params;
+                    }
+
+                };
+
+// Add the request to the queue
+                Volley.newRequestQueue(con).add(stringRequest);
+
+
+
+
+
+
+
+                //////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
+
+            }
+        });
+*/
+        //TextView year = (TextView) convertView.findViewById(R.id.releaseYear);
+
+        // getting movie data for the row
+
+        emll = m.getAemailid();
+
+        // thumbnail image
+    //    thumbNail.setImageUrl(AGlobal.url+m.getThumbnailUrl(), imageLoader);
+
+        // title
+        String d="";
+        if(Integer.parseInt(m.getStatus())==0)
+            d="accepted";
+        else
+            d = "rejected";
+        title.setText(m.getBemailid() +" has "+d+" your request");
+
+        // rating
+        //rating.setText("Source Address: " + String.valueOf(m.getRating()));
+
+        // genre
+        //String genreStr = "lol";
+        /*for (String str : m.getGenre()) {
+            genreStr += str + ", ";
+        }
+
+
+        genreStr = genreStr.length() > 0 ? genreStr.substring(0,
+                genreStr.length() - 2) : genreStr;*/
+     /*   title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AGlobal.em = emll;
+
+                Toast.makeText(con,emll,Toast.LENGTH_SHORT).show();
+                //FragmentManager fm = fml;
+                ///FragmentTransaction ft=fm.beginTransaction();
+                FragmentTransaction ft = ((FragmentActivity)con).getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.containerView,new LiftProfileViewFragment()).addToBackStack(null).commit();
+
+            }
+        });
+*/
+        // release year
+        //year.setText(String.valueOf(m.getYear()));
+
+        return convertView;
+    }
+}
